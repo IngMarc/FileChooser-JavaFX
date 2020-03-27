98e383462ad4902c7f3b111c456e12dae8a652be
marc2015 <marctsivanyo@gmail.com>
2020-03-27 23:29:02 +0100

    First commit to TreeView

---
+++ Documents/NetBeansProjects/TreeView/src/javaFx/Main.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaFx;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Main extends Application {
+    Image icon=new Image(getClass().getResourceAsStream("/img/folder.png"));
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.setTitle("Tree View!");
+        stage.getIcons().add(icon);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
---
+++ Documents/NetBeansProjects/TreeView/src/javaFx/MainController.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaFx;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class MainController implements Initializable {
+    
+    
+    @FXML
+    private TreeView<String> treeView;
+    
+    Image icon=new Image(getClass().getResourceAsStream("/img/folder.png"));
+                
+    private void handleButtonAction(ActionEvent event) {
+         
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+        TreeItem<String> root=new TreeItem<>("This PIC", new ImageView(icon));
+         
+         TreeItem<String> nodeA= new TreeItem<>("My Document", new ImageView(icon));
+         TreeItem<String> nodeB= new TreeItem<>("My Pictures", new ImageView(icon));
+         TreeItem<String> nodeC= new TreeItem<>("My Music", new ImageView(icon));
+         //SubAdd
+         TreeItem<String> nodeA1= new TreeItem<>("Workspace", new ImageView(icon));
+         TreeItem<String> nodeA2= new TreeItem<>("Downloads", new ImageView(icon));
+         TreeItem<String> nodeA3= new TreeItem<>("NetBeansProjects", new ImageView(icon));
+         
+         nodeA.getChildren().addAll(nodeA1,nodeA2,nodeA3);
+         root.getChildren().add(nodeA);         
+         root.getChildren().add(nodeB);
+         root.getChildren().add(nodeC);
+          
+         treeView.setRoot(root);
+    }    
+    
+}
